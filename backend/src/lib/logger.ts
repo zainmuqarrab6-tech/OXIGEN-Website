cd73f85e05b2a25a33cec8195f8ac02730cd3cc4
MaAn-41 <usmanamjad495@gmail.com>
2026-07-16 12:53:08 +0500

    Add backend logger

---
+++ backend/src/lib/logger.ts
@@ -0,0 +1,10 @@
+import pino from "pino";
+
+export const logger = pino({
+  level: process.env.LOG_LEVEL ?? "info",
+  redact: [
+    "req.headers.authorization",
+    "req.headers.cookie",
+    "res.headers['set-cookie']",
+  ],
+});
